d52984c8111934e41c6d4b2976575db51e736c9d
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2019-08-13 14:24:06 -0400

    Add 001 chapter file with section 1.1 and 1.2

---
+++ 001-getting-started-with-python-language.py
@@ -0,0 +1,21 @@
+# SECTION 1.1: GETTING STARTED
+
+"""
+Python 2 has a number of functionalities that can be optionally imported from Python 3 using the __future__
+module, as discussed here.
+
+P: 4
+"""
+
+from __future__ import print_function
+
+
+# SECTION 1.2: CREATING VARIABLES AND ASSIGNING VALUES
+
+"""
+You can not use python's keywords as a valid variable name. You can see the list of keyword by:
+P: 7
+"""
+
+import keyword
+print(keyword.kwlist)
